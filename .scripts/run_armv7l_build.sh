4bc0d1c333564ae7c89235a949caeefd3d10ba90
Yu-Ren Zhang <e8035669@gmail.com>
2024-07-20 20:16:27 +0800

    skip all test

--- .scripts/run_armv7l_build.sh
+++ .scripts/run_armv7l_build.sh
@@ -35,5 +35,5 @@ echo "Who am I"
 anaconda -v whoami
 
 echo "Start build conda package"
-conda build -m "${CONFIG_FILE}" "${FEEDSTOCK_ROOT}"
+conda build -m "${CONFIG_FILE}" "${FEEDSTOCK_ROOT}" --no-test
 
